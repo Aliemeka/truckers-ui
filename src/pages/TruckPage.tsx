4a4a6937fa338705da652c7dc34906b5b8bc1c0e
Emeka Allison <emekaallison4@gmail.com>
2021-10-30 11:32:44 +0100

    --amend

--- src/pages/TruckPage.tsx
+++ src/pages/TruckPage.tsx
@@ -45,7 +45,7 @@ const TruckPage = () => {
               />
               <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                 <h2 className="text-sm title-font text-gray-500 tracking-widest">
-                  {truck.name}
+                  SN: {truck.id}
                 </h2>
                 <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
                   {truck.name}
